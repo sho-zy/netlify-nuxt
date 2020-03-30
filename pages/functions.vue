156ae771d7f1d4d5ff1bbbc0bd9bd742907b6e02
sho-zy <j.shoji.0118@gmail.com>
2020-03-30 19:36:30 +0900

    Add Netlify Functions.

---
+++ pages/functions.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h2 v-text="'Netlify Functions × Nuxt.js サンプル'" />
+    <p v-text="message" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  asyncData() {
+    const baseUrl =
+      process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:9000'
+        : process.env.URL
+    return axios.get(baseUrl + '/.netlify/functions/hello').then((response) => {
+      return {
+        message: response.data
+      }
+    })
+  }
+}
+</script>
